e80045db7e74c792230918c56ff23d0eb33056f3
Fadli Maghfirli <89173845+fadlimaghfirli@users.noreply.github.com>
2025-06-24 03:06:17 +0700

    Create index.php

---
+++ Pekan 09/012-Fadli Maghfirli/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Sederhana</title>
+</head>
+<body>
+    <?php
+        // Variabel dasar
+        $nama = "Fadli";
+        $nim = 230631100012;
+        $prodi = "Pendidikan Informatika"
+        
+        // Tampilkan output
+        echo "Halo, nama saya $nama, dengan NIM $nim, dari Prodi $prodi";
+    ?>
+</body>
