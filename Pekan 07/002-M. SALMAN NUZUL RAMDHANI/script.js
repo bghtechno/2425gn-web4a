ee8be76e08c6fcf6dac500931f65617bfdb7e919
Aldi Salman Ramdhani <138585379+slmnldy@users.noreply.github.com>
2025-06-24 07:51:21 +0700

    Update script.js

--- Pekan 07/002-M. SALMAN NUZUL RAMDHANI/script.js
+++ Pekan 07/002-M. SALMAN NUZUL RAMDHANI/script.js
@@ -1,1 +1,19 @@
+// Menuliskan teks ke konsol
+console.log("Hai Dunia!");
+
+// Menampilkan kotak peringatan ke pengguna
+alert("Halo, selamat datang!");
+
+// Contoh penggunaan variabel
+let pengguna = "Andi";
+let usia = 20;
+
+// Contoh deklarasi fungsi
+function beriSalam(pengguna) {
+  return "Hai, " + pengguna + "!";
+}
+
+// Memanggil fungsi dan menampilkan hasilnya
+let hasilSalam = beriSalam(pengguna);
+console.log(hasilSalam);
 
