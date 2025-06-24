b3be8ba45f208d589f10bfceec8a0e4e4a730ff7
Aldi Salman Ramdhani <138585379+slmnldy@users.noreply.github.com>
2025-06-24 07:53:39 +0700

    Create index.php

---
+++ Pekan 09/002-M. SALMAN NUZUL RAMDHANI/index.php
@@ -0,0 +1,19 @@
+//    Contoh file PHP dasar
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh PHP</title>
+</head>
+<body>
+    <?php
+        // Deklarasi variabel
+        $namaLengkap = "Salman Ramdhani";
+        $nomorInduk = 230631100002;
+        $programStudi = "Pendidikan Informatika";
+
+        // Menampilkan informasi
+        echo "Perkenalkan, saya $namaLengkap, NIM $nomorInduk, dari program studi $programStudi.";
+    ?>
+</body>
+</html>
