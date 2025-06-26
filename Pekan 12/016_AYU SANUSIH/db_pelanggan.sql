eb51852a5082714d6c6a7e11d1464a3700bb5267
Ayusanusih25 <ayusanusih12@gmail.com>
2025-06-26 18:32:08 +0700

    Update db_pelanggan.sql

--- Pekan 12/016_AYU SANUSIH/db_pelanggan.sql
+++ Pekan 12/016_AYU SANUSIH/db_pelanggan.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE db_pelanggan;
 
+USE db_pelanggan;
+
+CREATE TABLE pelanggan (
+    id_Pelanggan INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100),
+    email VARCHAR(100),
+    telepon VARCHAR(20),
+    Alamat VARCHAR(20)
+);
