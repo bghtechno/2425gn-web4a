aaebb949c1ab7d552f49e5c7dce0d18e1f117d49
Ayusanusih25 <ayusanusih12@gmail.com>
2025-06-26 18:24:20 +0700

    Update index.php

--- Pekan 09/016_AYU SANUSIH/index.php
+++ Pekan 09/016_AYU SANUSIH/index.php
@@ -1,1 +1,19 @@
+//    Contoh file PHP dasar
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh PHP</title>
+</head>
+<body>
+    <?php
+        // Deklarasi variabel
+        $namaLengkap = "Ayu Sanusih";
+        $nomorInduk = 230631100016;
+        $programStudi = "Pendidikan Informatika";
+
+        // Menampilkan informasi
+        echo "Perkenalkan, saya $namaLengkap, NIM $nomorInduk, dari program studi $programStudi.";
+    ?>
+</body>
+</html>
