7b0a465cdad7e438b0b8b1d79224500c678fd610
Ayusanusih25 <ayusanusih12@gmail.com>
2025-06-26 18:35:47 +0700

    Update hapus.php

--- Pekan 12/016_AYU SANUSIH/hapus.php
+++ Pekan 12/016_AYU SANUSIH/hapus.php
@@ -1,1 +1,11 @@
+<?php
+include 'koneksi.php';
+$id = $_GET['id'];
 
+$delete = mysqli_query($koneksi, "DELETE FROM pelanggan WHERE id=$id");
+if ($delete) {
+    header("Location: index.php");
+} else {
+    echo "Gagal menghapus data.";
+}
+?>
