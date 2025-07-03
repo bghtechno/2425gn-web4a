78499d7e6d6b6d53d9e2e0fc76e9fe1ca6231e52
siboyarisona <siboyerisona@gmail.com>
2025-07-03 10:30:31 +0700

    Create index.php

---
+++ Pekan 09/011_SIBOY ARISONA/index.php
@@ -0,0 +1,17 @@
+/    Tugas membuat file PHP sederhana
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Sederhana</title>
+</head>
+<body>
+    <?php
+        // Variabel dasar
+        $nama = "Siboy Arisona";
+        $nim = 230631100011;
+        $prodi = "Pendidikan Informatika"
+        
+        // Tampilkan output
+        echo "Halo, nama saya $nama, dengan NIM $nim, dari Prodi $prodi";
+    ?>
+</body>
