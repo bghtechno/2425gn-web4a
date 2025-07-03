f7817720bb8830a964cc75268dbbd6ec955c7cef
siboyarisona <siboyerisona@gmail.com>
2025-07-03 10:27:57 +0700

    Update scrip.js

--- Pekan 07/011_SIBOY ARISONA/scrip.js
+++ Pekan 07/011_SIBOY ARISONA/scrip.js
@@ -1,1 +1,18 @@
+// Menampilkan pesan ke konsol
+console.log("Halo");
 
+// Menampilkan alert ke pengguna
+alert("Selamat datang!");
+
+// Contoh variabel
+let nama = "boy";
+let umur = 20;
+
+// Contoh fungsi
+function sapa(nama) {
+  return "Halo, " + nama + "!";
+}
+
+// Menggunakan fungsi
+let pesan = sapa(nama);
+console.log(pesan);
